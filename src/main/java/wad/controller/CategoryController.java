bf7113e8c16ab25dda218409a312e1ac673c6e9b
Kumara Saini M <skumara@lx7-fuxi124.ad.helsinki.fi>
2017-12-08 16:40:16 +0200

    category based navigation bar, article listing by category and category removing

--- src/main/java/wad/controller/CategoryController.java
+++ src/main/java/wad/controller/CategoryController.java
@@ -1,13 +1,19 @@
 package wad.controller;
 
+import java.util.Iterator;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import wad.domain.Article;
 import wad.domain.Category;
+import wad.repository.ArticleRepository;
 import wad.repository.CategoryRepository;
 
 @Controller
@@ -15,6 +21,9 @@ public class CategoryController {
     
     @Autowired
     private CategoryRepository categoryRepository;
+    
+    @Autowired
+    private ArticleRepository articleRepository;
 
     @GetMapping("/categories")
     public String listCategories(Model model) {
@@ -29,6 +38,7 @@ public class CategoryController {
         Category category = this.categoryRepository.getOne(id);
         model.addAttribute("category", category);
         model.addAttribute("articles", category.getArticles());
+        model.addAttribute("categories", this.categoryRepository.findAll());
         
         return "category";
     }
@@ -39,6 +49,20 @@ public class CategoryController {
         category.setName(name);
         this.categoryRepository.save(category);
 
+        return "redirect:/categories";
+    }
+    
+    @Transactional
+    @DeleteMapping("/categories/{id}")
+    public String removeCategory(@PathVariable Long id) {
+        Category category = this.categoryRepository.getOne(id);
+        
+        category.getArticles().forEach((article) -> {
+            this.articleRepository.getOne(article.getId()).getCategories().remove(category);
+        });
+        
+        this.categoryRepository.delete(category);
+        
         return "redirect:/categories";
     }
 }
