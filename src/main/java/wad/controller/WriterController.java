7c030263c9ce1ebdcb6bd7e09db86469443e03a3
Kumara Saini M <skumara@lx7-fuxi124.ad.helsinki.fi>
2017-12-08 16:43:48 +0200

    article listing by writer and writer removing

--- src/main/java/wad/controller/WriterController.java
+++ src/main/java/wad/controller/WriterController.java
@@ -2,24 +2,36 @@ package wad.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import wad.domain.Category;
 import wad.domain.Writer;
+import wad.repository.CategoryRepository;
 import wad.repository.WriterRepository;
+import wad.repository.ArticleRepository;
 
 @Controller
 public class WriterController {
     
     @Autowired
     private WriterRepository writerRepository;
+    
+    @Autowired
+    private CategoryRepository categoryRepository;
+    
+    @Autowired
+    private ArticleRepository articleRepository;
 
     @GetMapping("/writers")
     public String listCategories(Model model) {
 
         model.addAttribute("writers", this.writerRepository.findAll());
+        model.addAttribute("categories", this.categoryRepository.findAll());
         
         return "writers";
     }
@@ -39,6 +51,20 @@ public class WriterController {
         writer.setName(name);
         this.writerRepository.save(writer);
 
+        return "redirect:/writers";
+    }
+    
+    @Transactional
+    @DeleteMapping("/writers/{id}")
+    public String removeWriter(@PathVariable Long id) {
+        Writer writer = this.writerRepository.getOne(id);
+        
+        writer.getArticles().forEach((article) -> {
+            this.articleRepository.getOne(article.getId()).getWriters().remove(writer);
+        });
+        
+        this.writerRepository.delete(writer);
+        
         return "redirect:/writers";
     }
 }
