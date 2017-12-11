e356c904be2e116bec29c4cfd432f47b6f428dd5
Kumara Saini M <skumara@lx7-fuxi124.ad.helsinki.fi>
2017-12-11 22:34:48 +0200

    fix html errors

--- src/main/java/wad/controller/CategoryController.java
+++ src/main/java/wad/controller/CategoryController.java
@@ -1,7 +1,5 @@
 package wad.controller;
 
-import java.util.Iterator;
-import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
@@ -11,7 +9,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import wad.domain.Article;
 import wad.domain.Category;
 import wad.repository.ArticleRepository;
 import wad.repository.CategoryRepository;
