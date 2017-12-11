91073bd64863f26f09e67d3a6a2d31e634de2a13
Kumara Saini M <skumara@lx7-fuxi124.ad.helsinki.fi>
2017-12-11 17:16:29 +0200

    introduce login

---
+++ src/main/java/wad/config/SecurityConfiguration.java
@@ -0,0 +1,40 @@
+package wad.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        // mahdollistetaan h2-konsolin käyttö
+        http.csrf().disable();
+        http.headers().frameOptions().sameOrigin();
+
+        http.authorizeRequests()
+                .antMatchers("/h2-console/*").permitAll()
+                .antMatchers("/").permitAll()
+                .antMatchers("/articles/**").permitAll()
+                .antMatchers("/writers/**").permitAll()
+                .antMatchers("/categories/**").permitAll()
+                .antMatchers("/pictures/**").permitAll()
+                .antMatchers("/pictures_by_article/**").permitAll()
+                .anyRequest().authenticated().and()
+                .formLogin().permitAll().and()
+                .logout().permitAll();
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("bouldergod").password("grade").roles("USER");
+    }
+}
